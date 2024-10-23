3f9fcd6fc1d857ba97e49073dc2c528d65d76ed7
nrak126 <daichidaichi126@gmail.com>
2024-10-23 11:01:34 +0900

    04

---
+++ src/03/01-use_let_variable.js
@@ -0,0 +1,5 @@
+let price = 100;
+console.log(price);
+
+let name = 'alice';
+console.log(name);
---
+++ src/04/20-string_basic.js
@@ -0,0 +1,8 @@
+const a = "Hello world";
+console.log(a);
+
+const b = "こんにちは";
+console.log(b);
+
+console.log("Alice");
+console.log("Bob");
